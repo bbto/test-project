5c5d4e7f80632db0ac403e7725c30fdf41ba1ed6
Alvaro Insignares <bbto@koombea-dell1.(none)>
2009-12-06 22:45:39 -0500

    logout

--- sres/src/java/com/sres/servlet/LogoutServlet.java
+++ sres/src/java/com/sres/servlet/LogoutServlet.java
@@ -2,24 +2,22 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package com.sres.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import com.sres.persistence.DatabaseManager;
-import com.sres.model.Competition;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 /**
  *
  * @author bbto
  */
-public class AddCompetitionServlet extends HttpServlet {
-
+public class LogoutServlet extends HttpServlet {
+   
     /** 
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
@@ -28,24 +26,27 @@ public class AddCompetitionServlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        String name = request.getParameter("name");
-        DatabaseManager db = DatabaseManager.getInstance();
-        if (db != null) {
-            Competition competition = new Competition(true);
-            competition.setName(name);
-            if (competition.save()) {
-                response.sendRedirect(request.getContextPath() + "/admin/");
-            } else {
-                response.sendRedirect(request.getContextPath() + "/error.jsp");
-            }
-        } else {
-            response.sendRedirect(request.getContextPath() + "/error.jsp");
-        }
-    }
+        
+        ((HttpServletRequest) request).getSession().invalidate();
+        response.sendRedirect(request.getContextPath() + "/login.jsp");
+    } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
     /** 
      * Handles the HTTP <code>POST</code> method.
      * @param request servlet request
@@ -55,7 +56,7 @@ public class AddCompetitionServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         processRequest(request, response);
     }
 
@@ -67,4 +68,5 @@ public class AddCompetitionServlet extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
 }
