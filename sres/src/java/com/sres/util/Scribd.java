9e761a3c635b0149371caeb4842570100151a344
unknown <Chiquitica@.(none)>
2009-12-08 23:41:59 -0500

    Hp scribd por fin

--- sres/src/java/com/sres/util/Scribd.java
+++ sres/src/java/com/sres/util/Scribd.java
@@ -1,13 +1,29 @@
-package com.sres.util;
+package scribdupload;
 
-import com.sun.xml.rpc.processor.modeler.j2ee.xml.string;
+import com.sres.util.ClientHttpRequest;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.Set;
+import org.w3c.dom.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  *
- * @author bbto
+ * @author German Garcia
  */
+//TODO: This class needs a litle more work, and some documentation comments
 public class Scribd {
     /* Unofficial Scribd Java Class library */
 
@@ -15,8 +31,11 @@ public class Scribd {
     public String secret;
     private String url;
     public String session_key;
+    public String user_id;
     public String my_user_id;
-    private String error;
+
+    private String errorCode;
+    private String errorMessage;
 
     public Scribd(String api_key, String secret) {
         this.api_key = api_key;
@@ -27,27 +46,26 @@ public class Scribd {
     /**
      * Upload a document from a file
      * @param string file : relative path to file
-     * @param string doc_type : PDF, DOC, TXT, PPT, etc.
-     * @param string access : public or private. Default is Public.
-     * @param int rev_id : id of file to modify
-     * @return array containing doc_id, access_key, and secret_password if necessary.
+     * @param Hashtable optional : hash containing none or some of these parameters
+     * @optional string doc_type : PDF, DOC, TXT, PPT, etc.
+     * @optional string access : public or private. Default is Public.
+     * @optional int rev_id : id of file to modify
+     * @optional int{0,1} paid_content : when set to 1, the document becomes protected. Default is 0
+     * @optional int{0,1} secure : makes this document an iPaper Secure document
+     * @returns array containing doc_id, access_key, and secret_password if necessary.
      */
-    public Hashtable upload(String file, String doc_type, String access, String rev_id) {
+    public Hashtable upload(String file, Hashtable optional) {
         String method = "docs.upload";
         Hashtable params = new Hashtable();
-        if (doc_type != null) {
-            params.put("doc_type", doc_type);
+        params.put("file", file);
+        if(optional!=null) {
+            Set set = optional.keySet();
+            Iterator i = set.iterator();
+            while (i.hasNext()) {
+                params.put(i, optional.get(i));
+            }
         }
-        if (access != null) {
-            params.put("access", access);
-        }
-        if (file != null) {
-            params.put("file", file);
-        }
-        //$params['file'] = "@".$file;
-
-        Hashtable result = this.postRequest(method, params);
-        return result;
+        return this.postRequest(method, params);
     }
 
     /**
@@ -81,23 +99,23 @@ public class Scribd {
      * Get a list of the current users files
      * @return array containing doc_id, title, description, access_key, and conversion_status for all documents
      */
-    public Hashtable getList() {
+    public ArrayList getList() {
         String method = "docs.getList";
         Hashtable result = this.postRequest(method, null);
-        return result.get("resultset");
+        return (ArrayList) result.get("resultset");
     }
 
     /**
      * Get the current conversion status of a document
      * @param int $doc_id : document id
-     * @return string containing DISPLAYABLE", "DONE", "ERROR", or "PROCESSING" for the current document.
+     * @return string containing "DISPLAYABLE", "DONE", "ERROR", or "PROCESSING" for the current document.
      */
-    public Hashtable getConversionStatus(String doc_id) {
+    public String getConversionStatus(String doc_id) {
         String method = "docs.getConversionStatus";
         Hashtable params = new Hashtable();
         params.put("doc_id", doc_id);
-        String result = this.postRequest(method, params);
-        return result.get("conversion_status");
+        Hashtable result = this.postRequest(method, params);
+        return (String) result.get("conversion_status");
     }
 
     /**
@@ -161,7 +179,7 @@ public class Scribd {
      * @param string $scope : scope of search, "all" or "user"
      * @return array of results, each of which contain doc_id, secret password, access_key, title, and description
      */
-    public Hashtable search(String query, String num_results,
+    public ArrayList search(String query, String num_results,
             String num_start, String scope) {
         String method = "docs.search";
         Hashtable params = new Hashtable();
@@ -170,7 +188,7 @@ public class Scribd {
         params.put("num_start", num_start);
         params.put("scope", scope);
         Hashtable result = this.postRequest(method, params);
-        return result.get("result_set");
+        return (ArrayList) result.get("result_set");
     }
 
     /**
@@ -179,14 +197,18 @@ public class Scribd {
      * @param string $password : password of user to log in
      * @return array containing session_key, name, username, and user_id of the user;
      */
-    public Hashtable login(String username, String password) {
+    public boolean login(String username, String password) {
         String method = "user.login";
         Hashtable params = new Hashtable();
         params.put("username", username);
         params.put("password", password);
         Hashtable result = this.postRequest(method, params);
-        this.session_key = result.get("session_key");
-        return result;
+        if (result!=null && result.containsKey("user_id")) {
+            this.user_id = (String) result.get("user_id");
+            this.session_key = (String) result.get("session_key");
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -209,605 +231,121 @@ public class Scribd {
     }
 
     private Hashtable postRequest(String method, Hashtable params) {
+        Hashtable result = null;
+
         params.put("method", method);
-        params.put("session_key", session_key);
-        params.put("my_user_id", my_user_id);
+        if (session_key != null) {
+            params.put("session_key", session_key);
+        }
+        if (my_user_id != null) {
+            params.put("my_user_id", my_user_id);
+        }
 
         Hashtable post_params = new Hashtable();
-        Iterator i = params.iterator();
+        Set<String> set = params.keySet();
+        Iterator i = set.iterator();
         while (i.hasNext()) {
-            id(params.get("")
-            str = itr.next();
-            System.out.println(str + ": " + balance.get(str));
-        }
-        foreach($params as $key = > & $val){ 
-                if (!empty($val)){
-                    if (is_array($val)) $val = implode(',', $val);
+            Object key = i.next();
+            String val = params.get(key).toString();
+            if (!val.isEmpty()) {
+                if (!key.toString().equals("file") && val.startsWith("@")) {
+                    val = (char) (32) + val;
                 }
-                if ($key != 'file' && substr($val, 0, 1) == "@") {
-                    $val = chr(32).$val;
-                }
-                $post_params[$key] = $val;
+                post_params.put(key, val);
             }
         }
-        $secret = $this ->  secret;
-        $post_params['api_sig']
-        = $this->generate_sig($params, $secret);
-        $request_url = $this ->  url;
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $request_url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_params);
-        $xml = curl_exec($ch);
-        $result = simplexml_load_string($xml);
-        curl_close($ch);
-
-        if ($result['stat' {
-            
-        }
-        ] == 'fail')
-        {
-
-            //This is ineffecient.
-            $error_array = (array) $result;
-            $error_array = (array) $error_array;
-            $error_array = (array) $error_array['error'
-              ];
-				$error_array = $error_array[
-            '@attributes']
-              ;
-				$this- > error = $error_array['code'
-              ];
-
-				throw new Exception($error_array[
-            'message']
-            , $error_array['code']
-             );
-
-				return 0;
-
-        }
-        if ($result['stat' {
-            
-        }
-        
-                ] == "ok"){
-				
-				//This is shifty. Works currently though.
-				$result = $this- > convert_simplexml_to_array($result);
-            if (urlencode((string) $result) == "%0A%0A" && $this ->  error == 0) {
-                $result = "1";
-                return $result;
-            } else {
-                return $result;
+        if (!post_params.containsKey("api_sig")) {
+            String md5 = generate_sig(params, secret);
+            if (md5 != null) {
+                post_params.put("api_sig", md5);
             }
         }
-    }
+        System.out.println(post_params.toString());
 
-    params.put ("method"
-
-     , method);
-        params.put(
-
-     "session_key", session_key);
-        params.put(
-
-
-
-         "my_user_id", my_user_id);
-        $post_params = array();
-        foreach($params 
-        as $key = > &  $val
-
-             ) {
-            if (  {
-            !empty($val))
-
-            {
-                if (is_array($val)) {
-                    $val = implode(',', $val);
+        try {
+            ClientHttpRequest request = new ClientHttpRequest(url);
+            Set<String> pset = post_params.keySet();
+            i = pset.iterator();
+            while (i.hasNext()) {
+                Object key = i.next();
+                String val = post_params.get(key).toString();
+                if (key.toString().equals("file")) {
+                    request.setParameter(key.toString(), new File(val));
+                } else {
+                    request.setParameter(key.toString(), val);
                 }
             }
-            if ($key != 'file' {
-                
-            }
-            {
-            }
-            {
-            }
-            {
-            }
-            {
-            }
-            {
-            }
-            {
-            }
-            {
-            }
-            {
-            }
-            {
-            }
-            {
-            }
-            {
-            }
-            && substr($val, 0, 1) == "@"){
+            InputStream is = request.post();
+            /*BufferedReader bf = new BufferedReader(new InputStreamReader(is));
+            System.out.println(bf.readLine());
+            System.out.println(bf.readLine());
+            System.out.println(bf.readLine());
+            System.out.println(bf.readLine());
+            System.out.println(bf.readLine());*/
 
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(is);
 
-
-
-
-
-
-
-
-
-
-
-                $val = chr(32).$val;
+            String status = doc.getFirstChild().getAttributes().getNamedItem("stat").getNodeValue();
+            if (status.equals("ok")) {
+                result = new Hashtable();
+                NodeList nodes = doc.getFirstChild().getChildNodes();
+                for (int index = 0; index < nodes.getLength(); index++) {
+                    Node node = nodes.item(index);
+                    if (node.getNodeType() == Node.ELEMENT_NODE) {
+                        result.put(node.getNodeName(), node.getTextContent());
+                        System.out.println("JODA: " + node.getNodeName() + " = " + node.getTextContent());
+                    }
+                }
             }
-
-            $post_params[$key] = $val;
+            if (status.equals("fail")) {
+                NodeList nodes = doc.getFirstChild().getChildNodes();
+                for (int index = 0; index < nodes.getLength(); index++) {
+                    Node node = nodes.item(index);
+                    if (node.getNodeType() == Node.ELEMENT_NODE) {
+                        Node attr1 = node.getAttributes().item(0);
+                        Node attr2 = node.getAttributes().item(1);
+                        this.errorCode = attr1.getNodeValue();
+                        this.errorMessage = attr2.getNodeValue();
+                    }
+                }
+            }
+        } catch (SAXException ex) {
+            ex.printStackTrace(System.err);
+        } catch (IOException ex) {
+            ex.printStackTrace(System.err);
+        } catch (ParserConfigurationException ex) {
+            ex.printStackTrace(System.err);
         }
+        return result;
     }
-    $secret  = $this ->  secret;
-    $post_params
 
-
-
-
-         ['api_sig']
-        = $this->generate_sig($params, $secret);
-        $request_url = $this - > url;
-    $ch  = curl_init();
-
-    curl_setopt($ch, CURLOPT_URL, $request_url);
-
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-    curl_setopt($ch, CURLOPT_POST, 1);
-
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_params);
-    $xml  = curl_exec($ch);
-    $result  = simplexml_load_string($xml);
-
-    curl_close($ch);
-    if
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    ($result['stat' {
-            
+    public String generate_sig(Hashtable params_array, String secret) {
+        String str = "";
+        Set<String> set = params_array.keySet();
+        Iterator i = set.iterator();
+        // No estoy seguro de si seben ordenar los parametros
+        while (i.hasNext()) {
+            Object key = i.next();
+            String val = params_array.get(key).toString();
+            if (!key.toString().equals("file")) {
+                str = str + (key.toString() + val);
+            }
         }
-        {
+        str = secret + str;
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException ex) {
+            ex.printStackTrace(System.err);
+            return null;
         }
-        {
+        md.update(str.getBytes(), 0, str.length());
+        String md5 = new BigInteger(1, md.digest()).toString(16);
+        while (md5.length() < 32) {
+            md5 = "0" + md5;
         }
-        {
-        }
-        {
-        }
-        {
-        }
-        {
-        }
-        {
-        }
-        {
-        }
-        {
-        }
-        {
-        }
-
-
-
-
-
-
-           ]
-        == 'fail')
-        {
-
-            //This is ineffecient.
-            $error_array = (array) $result;
-            $error_array = (array) $error_array;
-            $error_array = (array) $error_array[
-        'error'
-
-          ];
-				$error_array = $error_array[
-
-
-
-
-
-
-
-
-
-
-
-        '@attributes']
-          ;
-				$this-            > error = $error_array['code'
-          ];
-
-				throw new Exception($error_array[
-
-
-
-
-
-
-
-
-
-
-
-        'message']
-        , $error_array['code']
-         );
-
-				return 0;
-
+        return md5;
     }
-    if
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    ($result['stat' {
-            
-        }
-        {
-        }
-        {
-        }
-        {
-        }
-        {
-        }
-        {
-        }
-        {
-        }
-        {
-        }
-        {
-        }
-        {
-        }
-        {
-        }
-
-
-
-
-            ]
-
-
-
-
-
-
-
-
-
-
-               == "ok"){
-
-				//This is shifty. Works currently though.
-				$result = $this-            > convert_simplexml_to_array($result);
-        if (urlencode((string) $result) == "%0A%0A" && $this ->  error == 0) {
-            $result = "1";
-            return $result;
-        } else {
-            return $result;
-        }
-    }
-}
-
-public static function generate_sig(
-
-$params_array, $secret) {
-        $str = ''
-        ;
-
-        ksort($params_array
-
-);
-        // Note: make sure that the signature parameter is not already included in
-        //       $params_array.
-        foreach   ($params_array
-
-as $k=           > $v
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-         )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        {
-        $str
-
-
-               .= $k . $v;
-		}
-
-$str = $secret . $str;
-
-		return
-
-md5($str);
-	}
-
-public static function convert_simplexml_to_array(
-
-$sxml) {
-		$arr = array();
-		if
-
-($sxml) {
-		  foreach ($sxml as $k =           > $v
-
-
-                 
-                 
-                 
-
-
-
-                 
-                 
-                 
-                 ) {
-                    if($arr[$k]){
-					$arr[$k." "
-                    .(count($arr) + 1)]
-                    = self:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-          : convert_simplexml_to_array($v);
-				}
-
-else{
-					$arr[$k] = self::convert_simplexml_to_array($v);
-				}
-
-}
-		}
-		if (sizeof($arr) > 0) {
-            return $arr;
-        }
-
-else {
-            return (string) $sxml;
-        }
-}
-}
-
 }
